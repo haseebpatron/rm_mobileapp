66d2aae5e066a704fd87aa32951a85ddae80c378
Haseeb <haseeb.patron@yahoo.com>
2018-08-22 00:43:25 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { RoadNetworkPage } from '../pages/road-network/road-network';
+import { RoadDetailsPage } from '../pages/road-details/road-details';
+import { RoadFurdetailsPage } from '../pages/road-furdetails/road-furdetails';
 import { ListPage } from '../pages/list/list';
+import { SettingsPage } from '../pages/settings/settings';
+
+export interface MenuItem {
+  title: string;
+  component: any;
+  icon: string;
+}
 
 @Component({
   templateUrl: 'app.html'
@@ -12,17 +23,22 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = LoginPage;
 
-  pages: Array<{title: string, component: any}>;
+  appMenuItems: Array<MenuItem>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+    this.appMenuItems = [
+      {title: 'Home', component: HomePage, icon: 'home'},
+      {title: 'Road Network', component: RoadNetworkPage, icon: 'document'},
+      {title: 'Bridges', component: HomePage, icon: 'document'},
+      {title: 'Rest Areas', component: HomePage, icon: 'document'},
+      {title: 'Pavements', component: HomePage, icon: 'document'},
+      {title: 'Traffic Services', component: HomePage, icon: 'document'},
+      {title: 'Settings', component: SettingsPage, icon: 'cog'}
     ];
 
   }
@@ -41,4 +57,9 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+
+  logout() {
+    this.nav.setRoot(LoginPage);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 import { ListPage } from '../pages/list/list';
+import { RegisterPage } from '../pages/register/register';
+import { RoadNetworkPage } from '../pages/road-network/road-network';
+import { RoadDetailsPage } from '../pages/road-details/road-details';
+import { RoadFurdetailsPage } from '../pages/road-furdetails/road-furdetails';
+import { SettingsPage } from '../pages/settings/settings';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
+    LoginPage,
+    RegisterPage,
+    RoadNetworkPage,
+    RoadDetailsPage,
+    RoadFurdetailsPage,
+    SettingsPage,
     ListPage
   ],
   imports: [
@@ -23,11 +37,18 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
+    LoginPage,
+    RegisterPage,
+    RoadNetworkPage,
+    RoadDetailsPage,
+    RoadFurdetailsPage,
+    SettingsPage,
     ListPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
